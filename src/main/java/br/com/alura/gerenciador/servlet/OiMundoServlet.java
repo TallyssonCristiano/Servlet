77ee56620c6a50b14ef9db0595d88e7b3037a5fc
Tallysson Cristiano <tallyssonand1@gmail.com>
2021-10-28 11:26:24 -0300

    Projeto em desenvolvimento

---
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,35 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+
+public class OiMundoServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Olá Mundo Servlet 2");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("ola mundo servlet 2");
+		
+	}
+
+}
